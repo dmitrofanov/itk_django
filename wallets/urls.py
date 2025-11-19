4e78943a46df1baaf80c49fc619011d68ccc22ff
Dmitry Mitrofanov <mail@dmitrof.com>
2025-11-20 00:36:33 +0300

    Change urls order

--- wallets/urls.py
+++ wallets/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'wallets'
 
 urlpatterns = [
-    path('wallets/<uuid:wallet_uuid>/', views.wallet_detail, name='wallet-detail'),
     path('wallets/<uuid:wallet_uuid>/operation/', views.wallet_operation, name='wallet-operation'),
+    path('wallets/<uuid:wallet_uuid>/', views.wallet_detail, name='wallet-detail'),
 ]
 
 
